0a5d94acce0b9f952e95f70b2a117b1e63bc3b20
krystianbialy <krystian.bialy@onet.pl>
2019-12-02 10:50:13 +0100

    answers clickable disabled/enabled & new variables/array & fixes

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -3,7 +3,11 @@ var quizAnswer1 = document.getElementById('quiz__answers-list-item-1');
 var quizAnswer2 = document.getElementById('quiz__answers-list-item-2');
 var quizAnswer3 = document.getElementById('quiz__answers-list-item-3');
 var quizAnswer4 = document.getElementById('quiz__answers-list-item-4');
-var score = document.getElementById('quiz__score-text-value');
+var quizAllAnswers = [quizAnswer1, quizAnswer2, quizAnswer3, quizAnswer4];
+var quizNextQuestionButton = document.getElementById(
+  'quiz__next-question-button'
+);
+var quizScore = document.getElementById('quiz__score-text-value');
 
 var quiz = {
   sets: [
@@ -20,65 +24,94 @@ var quiz = {
     {
       question: 'How is document type initialized in HTML5.?',
       options: [
-        '</DOCTYPE HTML>',
-        '</DOCTYPE>',
-        '<!DOCTYPE html>',
-        '</DOCTYPE html>'
+        '<xmp></DOCTYPE HTML></xmp>',
+        '<xmp></DOCTYPE></xmp>',
+        '<xmp><!DOCTYPE html></xmp>',
+        '<xmp></DOCTYPE html></xmp>'
       ],
       answer: 3
     },
     {
       question:
         'Which of the following HTML Elements is used for making any text bold ? ',
-      options: ['<p>', '<i>', '<li>', '<b>'],
+      options: [
+        '<xmp><p></xmp>',
+        '<xmp><i></xmp>',
+        '<xmp><li></xmp>',
+        '<xmp><b></xmp>'
+      ],
       answer: 4
     },
     {
       question:
         'Which of the following HTML element is used for creating an unordered list? ',
-      options: ['<ui>', '<i>', '<em>', '<ul>'],
+      options: [
+        '<xmp><ui></xmp>',
+        '<xmp><i></xmp>',
+        '<xmp><em></xmp>',
+        '<xmp><ul></xmp>'
+      ],
       answer: 4
     }
   ],
-  index: 0,
+  setIndex: 0,
   score: 0,
-  load: function() {
-    quizQuestion.innerHTML = this.sets[this.index].question;
-    quizAnswer1.innerHTML = this.sets[this.index].options[0];
-    quizAnswer2.innerHTML = this.sets[this.index].options[1];
-    quizAnswer3.innerHTML = this.sets[this.index].options[2];
-    quizAnswer4.innerHTML = this.sets[this.index].options[3];
-    score.innerHTML = this.score + '/' + this.sets.length;
+  loadQuizSet: function() {
+    quizQuestion.innerHTML = this.sets[this.setIndex].question;
+    quizAnswer1.innerHTML = this.sets[this.setIndex].options[0];
+    quizAnswer2.innerHTML = this.sets[this.setIndex].options[1];
+    quizAnswer3.innerHTML = this.sets[this.setIndex].options[2];
+    quizAnswer4.innerHTML = this.sets[this.setIndex].options[3];
+    quizScore.innerHTML = this.score + '/' + this.sets.length;
   },
-  check: function(answerId) {
+  nextQuestion: function() {
+    this.setIndex += 1;
+    this.loadQuizSet();
+  },
+  checkAnswer: function(answerId) {
     var id = answerId.split('');
     var test1 = id[id.length - 1];
-    var test2 = this.sets[this.index].answer;
+    var test2 = this.sets[this.setIndex].answer;
     console.log(test1);
     console.log(test2);
-    if (id[id.length - 1] == this.sets[this.index].answer) {
+    if (id[id.length - 1] == this.sets[this.setIndex].answer) {
       console.log('GOOD');
       this.score += 1;
-      this.scoreRefresh();
+      this.quizScoreRefresh();
       console.log('Score +' + this.score);
     } else {
       console.log('BAD');
     }
   },
-  scoreRefresh: function() {
-    score.innerHTML = this.score + '/' + this.sets.length;
+  answerNotClickable: function() {
+    var quizAnswers = quizAllAnswers;
+    for (var number = 0; number < quizAnswers.length; number += 1) {
+      quizAnswers[number].style.pointerEvents = 'none';
+    }
+  },
+  answerClickable: function() {
+    var quizAnswers = quizAllAnswers;
+    for (var number = 0; number < quizAnswers.length; number += 1) {
+      quizAnswers[number].style.pointerEvents = 'auto';
+    }
+  },
+  quizScoreRefresh: function() {
+    quizScore.innerHTML = this.score + '/' + this.sets.length;
   }
 };
 
-window.onload = quiz.load();
+window.onload = quiz.loadQuizSet();
 
-var quizAnswers = document.querySelectorAll(
-  '#quiz__answers-list-item-1, #quiz__answers-list-item-2, #quiz__answers-list-item-3, #quiz__answers-list-item-4'
-);
-
+var quizAnswers = quizAllAnswers;
 for (var number = 0; number < quizAnswers.length; number += 1) {
   quizAnswers[number].onclick = function() {
     var answerIdName = this.id;
-    quiz.check(answerIdName);
+    quiz.checkAnswer(answerIdName);
+    quiz.answerNotClickable();
   };
 }
+
+quizNextQuestionButton.onclick = function() {
+  quiz.nextQuestion();
+  quiz.answerClickable();
+};
